f230f9737af018253be78dcffd6ad0751de66864
boatengjulius14 <boatengjulius14@gmail.com>
2023-03-08 23:30:20 +0000

    islower

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper_str = ""
+    for i in str:
+        if i > 'Z':
+            upper_str += chr(ord(i) - 32)
+        else:
+            upper_str += i
+    print(upper_str)
