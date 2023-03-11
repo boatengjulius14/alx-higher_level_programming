73bc8b116057b10e7986703cffaef955f56b390a
boatengjulius14 <boatengjulius14@gmail.com>
2023-03-11 21:42:55 +0000

    Build my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ from calculator_1 import add, sub, mul, div
 def calculator(argv):
     if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
     a = int(argv[1])
     op = argv[2]
@@ -22,7 +22,7 @@ def calculator(argv):
             result = div(a, b)
         case _:
             print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+            exit(1)
     print("{:d} {:s} {:d} = {:d}".format(a, op, b, result))
 
 
