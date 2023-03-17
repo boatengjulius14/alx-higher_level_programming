87879ca59d2bde08f21face88fc7a4e359e1792f
boatengjulius14 <boatengjulius14@gmail.com>
2023-03-17 19:59:21 +0000

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    Max = max(a_dictionary.values())
+    for i in a_dictionary:
+        if a_dictionary[i] == Max:
+            return i
+    return None
