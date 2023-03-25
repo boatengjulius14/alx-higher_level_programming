47874b6be5b5332a91e06740d485734d4d0c65ee
boatengjulius14 <boatengjulius14@gmail.com>
2023-03-25 08:48:23 +0000

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,20 +5,18 @@
  * @list: pointer to head
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *check;
+	listint_t *fast, *slow;
 
-	if (!list || !list->next)
-		return (0);
+	fast = list;
+	slow = list;
 
-	check = list;
-
-	while (list)
+	while (slow && fast)
 	{
-		list = list->next;
-		if (check == list)
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
 			return (1);
 	}
 	return (0);
