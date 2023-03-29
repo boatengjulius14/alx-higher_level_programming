63379082896e409edd6b23d20f6276e8cac13393
boatengjulius14 <boatengjulius14@gmail.com>
2023-03-29 20:26:22 +0000

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """constructor with private attribute __size"""
+    def __init__(self, size=0):
+        """Initialize private attribute with size
+        Args:
+            size (int): size of a square.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
