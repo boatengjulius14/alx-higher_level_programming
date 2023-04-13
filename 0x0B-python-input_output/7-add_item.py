eed5c814e114a1118c391dc588774cb2cd88d914
boatengjulius14 <boatengjulius14@gmail.com>
2023-04-13 21:08:21 +0000

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,16 +2,18 @@
 """Adds all arguments to a python list, and then save them to a file"""
 
 
-import sys
-import os
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    import sys
+    import os
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_\
+json_file').load_from_json_file
 
-_list = []
-if os.path.exists('add_item.json'):
-    _list = load_from_json_file('add_item.json')
-    _list.extend(sys.argv[1:])
-    save_to_json_file(_list, 'add_item.json')
-else:
-    with open('add_item.json', 'w') as file:
+    _list = []
+    if os.path.exists('add_item.json'):
+        _list = load_from_json_file('add_item.json')
+        _list.extend(sys.argv[1:])
         save_to_json_file(_list, 'add_item.json')
+    else:
+        with open('add_item.json', 'w') as file:
+            save_to_json_file(_list, 'add_item.json')
