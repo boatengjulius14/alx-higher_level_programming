702d09d7e89a7127442849f5a039e5f1d479bfa2
boatengjulius14 <boatengjulius14@gmail.com>
2023-04-13 22:28:34 +0000

    Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines the function pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """returns a list of list of integers"""
+    
+    _list = [[]]
+    for i in range(n):
+        if i == 0:
+            _list = [[1]]
+        elif i == 1:
+            _list.extend([[1, 1]])
+        else:
+            add = 0
+            flag = 0
+            _listadd = [1]
+            j = 0
+            while j < len(_list[-1]):
+                add += _list[-1][j]
+                flag += 1
+                if flag == 2:
+                    _listadd.extend([add])
+                    flag = 0
+                    add = 0
+                    continue
+                j += 1
+            _listadd.append(1)
+            _list.extend([_listadd])
+    return _list
