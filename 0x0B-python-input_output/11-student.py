eb6bffc55a8635daa9d81bb02a2db4c0330413cb
boatengjulius14 <boatengjulius14@gmail.com>
2023-04-14 00:59:29 +0000

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines class Student"""
+
+
+class Student:
+    """Definition of Student class"""
+    def __init__(self, first_name, last_name, age):
+        """constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary representation of
+        an instance"""
+        if isinstance(attrs, list):
+            if all(isinstance(i, str) for i in attrs):
+                return {i: self.__dict__[i] for i in
+                        attrs if i in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for i in json:
+            self.i = json[i]
