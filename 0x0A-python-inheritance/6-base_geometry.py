4e7372ae9e9325506cacd5109598a6066c5022ec
boatengjulius14 <boatengjulius14@gmail.com>
2023-04-14 05:15:21 +0000

    Same class or inherit from

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """An empty class BaseGeometry"""
+
+    def area(self):
+        """raises Exception"""
+        raise Exception("area() is not implemented")
