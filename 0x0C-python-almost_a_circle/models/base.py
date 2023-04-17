faccb100ad07559effd746c1e7fa819f18f7dd4c
boatengjulius14 <boatengjulius14@gmail.com>
2023-04-17 23:42:06 +0000

    Let's draw it

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Base Class Module"""
 import json
+import csv
+import turtle
 
 
 class Base:
@@ -62,3 +64,72 @@ class Base:
             return [cls.create(**attr) for attr in ins_list]
         except IOError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Transfers serialized(csv) data into
+        a file
+        """
+        with open(f"{cls.__name__}.csv", "w", newline="") as file:
+            if list_objs is None or list_objs == []:
+                file.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    attrlist = ['id', 'width', 'height', 'x', 'y']
+                else:
+                    attrlist = ['id', 'size', 'x', 'y']
+                writer = csv.DictWriter(file, attrlist)
+                for ins in list_objs:
+                    writer.writerow(ins.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """returns deserialized(csv) list of instances"""
+        try:
+            with open(f"{cls.__name__}.csv", "r", newline="") as file:
+                if cls.__name__ == "Rectangle":
+                    attrlist = ['id', 'width', 'height', 'x', 'y']
+                else:
+                    attrlist = ['id', 'size', 'x', 'y']
+                reader = csv.DictReader(file, attrlist)
+                reader = [dict([i, int(j)] for i, j in elem.items())
+                          for elem in reader]
+                return [cls.create(**elem) for elem in reader]
+        except IOError:
+            return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Draw all the Rectangles and Squares"""
+        t_obj = turtle.Turtle()
+        t_obj.screen.bgcolor("#04bf1a")
+        t_obj.pensize(3)
+        t_obj.shape("turtle")
+
+        t_obj.color("#fff600")
+        for rec_obj in list_rectangles:
+            t_obj.showturtle()
+            t_obj.up()
+            t_obj.goto(rec_obj.x, rec_obj.y)
+            t_obj.down()
+            for _ in range(2):
+                t_obj.forward(rec_obj.width)
+                t_obj.left(90)
+                t_obj.forward(rec_obj.height)
+                t_obj.left(90)
+            t_obj.hideturtle()
+
+        t_obj.color("#b5e3d8")
+        for sq_obj in list_squares:
+            t_obj.showturtle()
+            t_obj.up()
+            t_obj.goto(sq_obj.x, sq_obj.y)
+            t_obj.down()
+            for _ in range(2):
+                t_obj.forward(sq_obj.width)
+                t_obj.left(90)
+                t_obj.forward(sq_obj.height)
+                t_obj.left(90)
+            t_obj.hideturtle()
+
+        turtle.exitonclick()
