18d6dea3c233e7fcb0cd58a8b4c2099ba2cdfdf8
boatengjulius14 <boatengjulius14@gmail.com>
2023-05-19 04:43:40 +0000

    1 to 16

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Creates the database htbn_0d_usa and the
+-- table states (in the database hbtn_0d_usa)
+-- on your MySQL server
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
