904834bdc53b2c6ff2131de5e378a74c5cdf9d41
boatengjulius14 <boatengjulius14@gmail.com>
2023-05-19 04:54:01 +0000

    1 to 16 EDITS

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,7 @@
 -- have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_show.title, tv_show_genres.genre_id;
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+GROUP BY tv_shows.title, tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
 -- Creates the database htbn_0d_usa and the
 -- table states (in the database hbtn_0d_usa)
 -- on your MySQL server
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 UNIQUE,
-    name VARCHAR(256)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
 );
