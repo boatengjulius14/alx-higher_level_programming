d34aee0b7cf253bcf236e028d24db56b16f0387b
boatengjulius14 <boatengjulius14@gmail.com>
2023-06-14 21:10:34 +0000

    Second Biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,23 +1,9 @@
 #!/usr/bin/node
+const args = process.argv.slice(2).map(Number);
 
-function findSecondLargest (arr) {
-  if (arr.length <= 3) {
-    return (0);
-  }
-
-  let max = arr[0];
-  let secondMax = arr[1];
-
-  for (let i = 2; i < arr.length; i++) {
-    if (arr[i] > max) {
-      secondMax = max;
-      max = arr[i];
-    } else if (arr[i] > secondMax) {
-      secondMax = arr[i];
-    }
-  }
-
-  return (secondMax);
+if (args.length > 1) {
+  const sortArr = args.sort((x, y) => y - x);
+  console.log(sortArr[1]);
+} else {
+  console.log(0);
 }
-
-console.log(findSecondLargest(process.argv.slice(2).map(Number)));
