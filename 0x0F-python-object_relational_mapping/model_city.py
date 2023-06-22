5f71b1f8e722b609cd8786c84a6f745c1e9d6d53
boatengjulius14 <boatengjulius14@gmail.com>
2023-06-22 20:31:34 +0000

    Cities in state

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Definition of the class City
+"""
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from model_state import Base
+
+
+class City(Base):
+    """Defines City class, a subclass of Base,
+    which creates an SQL table with  the name 'states' and columns,
+    id, name and state_id
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
