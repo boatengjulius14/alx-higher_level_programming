29d7c1776bf6307a5e0ea86f83758c3a2751d62f
boatengjulius14 <boatengjulius14@gmail.com>
2023-07-02 19:20:16 +0000

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #takes in a URL, sends a request to that URL, and displays the size fo the
 # body of the response
-curl -sw "\n%{size_download}\n" "$1" | tail -1
+curl -s -o /dev/null -w "%{size_download}" "$1"
